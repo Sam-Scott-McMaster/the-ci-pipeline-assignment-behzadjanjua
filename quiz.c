5211b2e63c5b2eff3495759874476deb50ae2a44
behzadjanjua <janjuab@mcmaster.ca>
2024-10-01 17:10:29 -0400

    "Errors previously, didn't mean to push in that state. Completed questions and answers"

--- quiz.c
+++ quiz.c
@@ -2,11 +2,9 @@
 #include <stdlib.h> //loads EXIT_SUCCESS and other important things
 
 int main() {
-    printf("Is the language C an Object-Oriented Language, or a Procedural Language?") //procedural
-    scanf()
-    printf("What Linux command displays file contents?") //cat
-    scanf()
-    printf("Which git command uploads the local repository content to a remote repository?") //git push 
+    printf("Is the language C an Object-Oriented Language, or a Procedural Language?\n"); //procedural
+    printf("What Linux command displays file contents?\n"); //cat
+    printf("Which git command uploads the local repository content to a remote repository?\n"); //git push 
     
     return EXIT_SUCCESS;
 }
