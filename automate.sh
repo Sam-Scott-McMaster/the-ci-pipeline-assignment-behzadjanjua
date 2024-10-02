7b40e0460fb0e78ee70bfb042cc81a637c4fa051
behzadjanjua <janjuab@mcmaster.ca>
2024-10-01 22:09:06 -0400

    Added git log, and comments to the top.

--- automate.sh
+++ automate.sh
@@ -1,3 +1,7 @@
+# Behzad Janjua, 400516103, October 3rd 2024
+#
+# This script adds, commits, tags (if specified), and pushes the quiz.c file. Alongside git diff and log (to track changes).
+
 git add quiz.c 
 git diff
 git commit
@@ -10,3 +14,4 @@ else
 fi
 git status
 git push
+git log
