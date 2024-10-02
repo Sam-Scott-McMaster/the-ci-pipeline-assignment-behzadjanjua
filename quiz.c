bd0b3e32124f7add01322621bcd04cb6ef0bb0e9
behzadjanjua <janjuab@mcmaster.ca>
2024-10-01 22:09:24 -0400

    "Completed the parts of the code to display questions."

--- quiz.c
+++ quiz.c
@@ -1,10 +1,27 @@
+/* Behzad Janjua, 400516103, October 3rd 2024
+*
+* This file is a quiz game. The user will be quizzed on three different questions. ADD 
+* MOREEEEEEEEEEEEEE
+*/
+
 #include <stdio.h> //input/output
 #include <stdlib.h> //loads EXIT_SUCCESS and other important things
+#include <string.h> //loads string comparison
 
-int main() {
-    printf("Is the language C an Object-Oriented Language, or a Procedural Language?\n"); //procedural
-    printf("What Linux command displays file contents?\n"); //cat
-    printf("Which git command uploads the local repository content to a remote repository?\n"); //git push 
+int main(int argc, char *argv[]) {
+    if (strcmp(argv[1], "-1") == 0) {
+        puts("Is the language C an Object-Oriented Language, or a Procedural Language?\n"); //procedural
+        return EXIT_SUCCESS;
+    }
     
-    return EXIT_SUCCESS;
+    else if (strcmp(argv[1], "-2") == 0) {
+        puts("What Linux command displays file contents?\n"); //cat
+        return EXIT_SUCCESS;
+    }
+    
+    else if (strcmp(argv[1], "-3") == 0) {
+        puts("Which git command uploads the local repository content to a remote repository?\n"); //push 
+        return EXIT_SUCCESS;
+    }
+    
 }
