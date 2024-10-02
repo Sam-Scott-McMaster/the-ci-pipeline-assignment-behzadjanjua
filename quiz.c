4cb09e8b74ed3c8852c85992bcf0cbc43cb015f4
behzadjanjua <janjuab@mcmaster.ca>
2024-10-02 18:42:50 -0400

    "Added git --help, but still need to fix some logic issues with the question/asking"

--- quiz.c
+++ quiz.c
@@ -1,7 +1,7 @@
 /* Behzad Janjua, 400516103, October 3rd 2024
 *
-* This file is a quiz game. The user will be quizzed on three different questions. ADD 
-* MOREEEEEEEEEEEEEE
+* This file is a quiz game. The user will be quizzed on three different questions. Future additions will include
+* a help feature, and bad flags.
 */
 
 #include <stdio.h> //input/output
@@ -9,19 +9,51 @@
 #include <string.h> //loads string comparison
 
 int main(int argc, char *argv[]) {
+
     if (strcmp(argv[1], "-1") == 0) {
-        puts("Is the language C an Object-Oriented Language, or a Procedural Language?\n"); //procedural
+        puts("\nQuestion: Is the language C an Object-Oriented Language, or a Procedural Language?\n"); //procedural
+        if (argc > 2 && strcmp(argv[2], "Procedural") == 0) {
+            puts("Success, correct answer!");
+        }
+        else if (argc > 2 && strcmp(argv[2], "Procedural") == 1) {
+            puts("Incorrect attempt... better luck next time!");
+        }
         return EXIT_SUCCESS;
     }
     
     else if (strcmp(argv[1], "-2") == 0) {
-        puts("What Linux command displays file contents?\n"); //cat
+        puts("\nQuestion: What Linux command displays file contents?\n"); //cat
+        if (argc > 2 && strcmp(argv[2], "cat") == 0) { 
+            puts("Success, correct answer!");
+        } 
+        else if (argc > 2 && strcmp(argv[2], "cat") == 1) {
+            puts("Incorrect attempt... better luck next time!");
+        }
         return EXIT_SUCCESS;
     }
-    
+
+    //UNRELATED, ADD A GIT PUSH ORIGIN MAIN TO BASH SCRIPT
     else if (strcmp(argv[1], "-3") == 0) {
-        puts("Which git command uploads the local repository content to a remote repository?\n"); //push 
+        puts("\nQuestion: Which git command uploads the local repository content to a remote repository?\n"); //push 
+        if (argc > 2 && strcmp(argv[2], "push") == 0) { 
+            puts("Success, correct answer!");
+        } 
+        else if (argc > 2 && strcmp(argv[2], "push") == 1) {
+            puts("Incorrect attempt... better luck next time!");
+        }
         return EXIT_SUCCESS;
     }
-    
+    if (strcmp(argv[1], "--help") == 0) {
+        puts("\nAsking a Question:");
+        puts("./quiz -1             Question 1");
+        puts("./quiz -2             Question 2");
+        puts("./quiz -3             Question 3");
+        puts("\nAnswering a Question (Replace '' with ""):"); 
+        puts("./quiz -1 'answer'    Answering Question 1");
+        puts("./quiz -2 'answer'    Answering Question 1");
+        puts("./quiz -3 'answer'    Answering Question 1");
+        puts("\nAsking for Help:");
+        puts("quiz --help");
+        return EXIT_SUCCESS;
+    }
 }
