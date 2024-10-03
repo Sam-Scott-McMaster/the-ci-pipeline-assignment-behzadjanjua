90d4acce1bda0af52d1384f8324adc1fa58172c4
behzadjanjua <janjuab@mcmaster.ca>
2024-10-03 13:41:13 -0400

    "Fixed all logic error, will push soon with comments"

--- quiz.c
+++ quiz.c
@@ -4,35 +4,35 @@
 * a help feature, and bad flags.
 */
 
-#include <stdio.h> //input/output
+#include <stdio.h> //loads input/output
 #include <stdlib.h> //loads EXIT_SUCCESS and other important things
 #include <string.h> //loads string comparison
 
 int main(int argc, char *argv[]) {
 
-    if (argc == 1) {
+    if (argc == 1) { //If only ./quiz is run, then shows user how to use program.
         puts("\nUsage: quiz [-#] [<answer>]");
         puts("\nTry 'quiz --help' for more information.\n");
         return EXIT_FAILURE;
     }
     
-    if (strcmp(argv[1], "--help") == 0) {
-        puts("Usage: quiz [-#] [<answer>]");
-        puts("Try 'quiz --help' for more information.\n");
-        puts("\nAsking a Question:");
-        puts("./quiz -1             Question 1");
-        puts("./quiz -2             Question 2");
-        puts("./quiz -3             Question 3");
-        puts("\nAnswering a Question (Replace '' with ""):"); 
-        puts("./quiz -1 'answer'    Answering Question 1");
-        puts("./quiz -2 'answer'    Answering Question 1");
-        puts("./quiz -3 'answer'    Answering Question 1");
-        puts("\nAsking for Help:");
-        puts("quiz --help");
+    if (strcmp(argv[1], "--help") == 0) { //If --help is called, then outputs a message on how to use program. 
+        puts("Usage: quiz [-#] [<answer>]\n"
+            "Try 'quiz --help' for more information.\n"
+            "\nAsking a Question:\n"
+            "./quiz -1             Question 1\n"
+            "./quiz -2             Question 2\n"
+            "./quiz -3             Question 3\n"
+            "\nAnswering a Question (Replace '' with \"\"):\n"
+            "./quiz -1 'answer'    Answering Question 1\n"
+            "./quiz -2 'answer'    Answering Question 2\n"
+            "./quiz -3 'answer'    Answering Question 3\n"
+            "\nAsking for Help:\n"
+            "quiz --help\n");
         return EXIT_SUCCESS;
     }
     
-    if (argc == 2) {
+    if (argc == 2) { //if there are two arguments, check which question is being asked 
         if (strcmp(argv[1], "-1") == 0) {
             puts("\nQuestion: Is the language C an A:Object-Oriented Language, or B:Procedural Language?\n");
         }
@@ -40,16 +40,18 @@ int main(int argc, char *argv[]) {
             puts("\nQuestion: What Linux command displays file contents?\n");
         }
         else if (strcmp(argv[1], "-3") == 0){
-            puts("\nQuestion: Which git command uploads the local repository content to a remote repository?\n"); //push 
+            puts("\nQuestion: Which git command uploads the local repository content to a remote repository?\n");
         }
+        //If the argument provided is invalid, it will catch error as a bad flag
         else {
             puts("\nUsage: quiz [-#] [<answer>]");
             puts("\nTry 'quiz --help' for more information.\n");
+            return EXIT_FAILURE;
         }
         return EXIT_SUCCESS;
     }
 
-    if (argc == 3) {
+    if (argc >= 3) {
         if (strcmp(argv[1], "-1") == 0) {
             if (strcmp(argv[2], "B") == 0) {
                 puts("Success! Correct Answer");
@@ -57,12 +59,8 @@ int main(int argc, char *argv[]) {
             else if (strcmp(argv[2], "B") == 1) {
                 puts("Incorrect Answer");
             }
-            else {
-                puts("\nUsage: quiz [-#] [<answer>]");
-                puts("\nTry 'quiz --help' for more information.\n");
-            }
         }
-        
+
         else if (strcmp(argv[1], "-2") == 0) {
             if (strcmp(argv[2], "cat") == 0) {
                 puts("Success! Correct Answer");
@@ -79,6 +77,11 @@ int main(int argc, char *argv[]) {
             else if (strcmp(argv[2], "push") == 1) {
                 puts("Incorrect Answer");
             }
+        }
+        else {
+            puts("\nUsage: quiz [-#] [<answer>]");
+            puts("\nTry 'quiz --help' for more information.\n");
+            return EXIT_FAILURE;
         }
         return EXIT_SUCCESS;
     }
